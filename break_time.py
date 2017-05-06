82df8990829cf390fbbfc14e483fed1907893d96
Kurt <kurtanderson234@gmail.com>
2017-05-06 11:25:09 -0500

    Updated to use Pomodoro method

--- break_time.py
+++ break_time.py
@@ -2,9 +2,11 @@ import time
 import webbrowser
 
 # Loops 3 times, prompting the user to take a break with
-# their favorite music video
+# their favorite music video. Uses the Pomodoro timing method.
+# 25 minutes on, 5 minutes off. 
 
 x = 0
 for x in range(0,3):
-    time.sleep(3)
+    time.sleep(25 * 60)
     webbrowser.open("https://www.youtube.com/watch?v=ALj5MKjy2BU")
+    time.sleep(5 * 60);
