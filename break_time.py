19dc551ac8b0a515e1dc4ad044663e9cd9e6d25d
Kurt <kurtanderson234@gmail.com>
2017-05-06 11:15:11 -0500

    Initial Commit

---
+++ break_time.py
@@ -0,0 +1,10 @@
+import time
+import webbrowser
+
+# Loops 3 times, prompting the user to take a break with
+# their favorite music video
+
+x = 0
+for x in range(0,3):
+    time.sleep(3)
+    webbrowser.open("https://www.youtube.com/watch?v=ALj5MKjy2BU")
